37e2ebd70167b21b06c0d3f1588c83721f793aa6
Alysson <alyssos@hotmail.com>
2024-09-27 16:13:53 -0300

    style: structure organization

--- schemas/mongo.sh
+++ schemas/mongo.sh
@@ -1,9 +1,9 @@
-docker run --name mongodb -d mongo:latest
+docker run --name mongodb -d -p 27017:27017 mongo:latest
 docker exec -it mongodb mongo
 #robomongo & mongo express 27017
 
-docker run -it --rm --name mongo mongo:latest mongod
+#docker run -it --rm --name mongo mongo:latest mongod
 
-https://github.com/docker-library/mongo/issues/558
+#https://github.com/docker-library/mongo/issues/558
 
 docker exec -it mongodb mongosh
